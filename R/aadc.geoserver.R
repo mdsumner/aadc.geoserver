cf2ed2dc7650a61336c49922674ade24896ec6c7
Michael Sumner <mdsumner@gmail.com>
2023-07-18 15:02:14 +1000

    add various capabilities

--- R/aadc.geoserver.R
+++ R/aadc.geoserver.R
@@ -1,10 +1,43 @@
-aadc.geoserver <- function() {
-  "WFS:https://data.aad.gov.au/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities"
+##https://data.aad.gov.au/geoserver/web/
+#  WMS:https://data.aad.gov.au/geoserver/ows?service=WMS&version=1.3.0&request=GetCapabilities
+
+  ## not available
+  ## WMTS:https://data.aad.gov.au/geoserver/gwc/service/wmts?service=WMTS&version=1.1.1&request=GetCapabilities
+#  WFS:https://data.aad.gov.au/geoserver/ows?service=WFS&acceptversions=2.0.0&request=GetCapabilities
+
+#  WCS:https://data.aad.gov.au/geoserver/ows?service=WCS&acceptversions=2.0.1&request=GetCapabilities
+  ## WPS?
+  ##https://data.aad.gov.au/geoserver/ows?service=WPS&version=1.0.0&request=GetCapabilities
+
+
+
+aadc.geoserver <- function(type = c("WFS")) {
+  aadc <- c(WFS = "WFS:https://data.aad.gov.au/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities",
+            WMS = "WMS:https://data.aad.gov.au/geoserver/ows?service=WMS&version=1.3.0&request=GetCapabilities",
+            WCS = "WCS:https://data.aad.gov.au/geoserver/ows?service=WCS&acceptversions=2.0.1&request=GetCapabilities")
+  out <- switch(type,
+         WFS = aadc["WFS"],
+         WMS = aadc["WMS"],
+         WCS = aadc["WCS"],
+         all = aadc,
+         "not supported")
+  if (out[1L] == "not supported") stop(sprintf("'type=%s' is not supported, should be one of '%s' or 'all'", type,
+                                           paste(collapse = ", ", names(aadc))))
+
+  out
 }
 
-aadc_layers <- function() {
-  vapour::vapour_layer_names(aadc.geoserver())
+aadc_wfs_layers <- function() {
+  vapour::vapour_layer_names(aadc.geoserver("WFS"))
 }
+aadc_wms_layers <- function() {
+  vapour::vapour_sds_names(aadc.geoserver("WMS"))
+}
+aadc_wcs_layers <- function() {
+  vapour::vapour_sds_names(aadc.geoserver("WCS"))
+}
+
+
 
 layer_info <- function(layer) {
   info <- vapour::vapour_layer_info(aadc.geoserver(), layer)
